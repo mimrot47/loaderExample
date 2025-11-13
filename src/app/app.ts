6fcf02cf8f0800d9bbac5b6824fcfaae64d9ba23
mimrot47 <mimrot47@gmail.com>
2025-11-13 22:59:15 +0530

    ngx loading

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
+import { INgxLoadingConfig, NgxLoadingAnimationTypes } from './../ngx-loading/ngx-loading-config';
+
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxLoadingComponent } from '../ngx-loading/ngx-loading.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgxLoadingComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('loaderExample');
+  loading = false;
+  config: INgxLoadingConfig = {
+    animationType: NgxLoadingAnimationTypes.Circle,
+    backdropBackgroundColour: 'rgba(0,0,0,0.4)',
+    fullScreenBackdrop: true,
+    primaryColour: '#00BCD4',
+    secondaryColour: '#E91E63',
+    tertiaryColour: '#FFEB3B',
+  };
+
+  toggleLoading() {
+    this.loading = !this.loading;
+
+    if (this.loading) {
+      setTimeout(() => (this.loading = false), 3000); // auto hide after 3s
+    }
+  }
 }
